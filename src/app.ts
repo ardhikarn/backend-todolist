bde40dd63bbfcfb81276c11d67c45fdb9b8ac0a1
ardhikarn <ardhikarn@gmail.com>
2020-10-29 21:48:36 +0700

    set backend

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, {Request, Response} from 'express'
+import mongoose, { mongo } from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+const PORT = 8080
+
+const {
+  MONGODB_ATLAS_USERNAME,
+  MONGODB_ATLAS_PASSWORD,
+  MONGODB_ATLAS_DBNAME
+} = process.env
+
+const url = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@cluster0.pymgz.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`
+
+const option = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('You are Connected')
+})
+
+// mongoose connect
+mongoose.set('useFindAndModify', true)
+mongoose.connect(url, option).then(() => {
+  app.listen(PORT, () => {
+    console.info(`App is listening at http://localhost:${PORT}`)
+  })
+}).catch(error => { throw error })
+
