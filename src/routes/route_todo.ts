b6bb3e2e21b0a1ab4a052df2ea9452d15ff09f23
ardhikarn <ardhikarn@gmail.com>
2020-10-29 23:01:50 +0700

    add middleware

--- src/routes/route_todo.ts
+++ src/routes/route_todo.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express' 
 import { getTodos, getTodo, postTodo, putTodo, deleteTodo} from '../controllers/control_todo'
+import bodyParser from 'body-parser'
 
 const router = Router()
+const jsonParser = bodyParser.json()
 
 router.get('/api/todos', getTodos)
 router.get('/api/todo/:id', getTodo)
-router.post('/api/add-todo', postTodo)
-router.put('/api/update-todo/:id', putTodo)
-router.delete('/api/delete-todo/:id', deleteTodo)
+router.post('/api/add-todo', jsonParser, postTodo)
+router.put('/api/update-todo/:id', jsonParser, putTodo)
+router.delete('/api/delete-todo/:id', jsonParser, deleteTodo)
 
 export default router
